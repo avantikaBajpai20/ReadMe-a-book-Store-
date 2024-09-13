c3018d2a238f0f69cae079fd625290ed84c8b031
avantikaBajpai20 <119681103+avantikaBajpai20@users.noreply.github.com>
2024-09-13 22:37:30 +0530

    Your descriptive message

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());//parsing in json
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+const URI = process.env.MONGODBURI;
 
 // connect to mongoDB
 try {
